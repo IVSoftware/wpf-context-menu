0645c4326bd88b3ef78e5aaf535b41c67fee1903
Tom Gregor <tgregor@ivsoftware.com>
2024-10-01 13:09:46 -0600

    Add project files.

---
+++ wpf-context-menu/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace wpf_context_menu.Converters
+{
+    public class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public Visibility VisibilityForFalse { get; set; } = Visibility.Hidden;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            Equals(value, true) ? Visibility.Visible : VisibilityForFalse;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            Equals(value, Visibility.Visible) ? true : false;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
---
+++ wpf-context-menu/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace wpf_context_menu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+    partial class MainWindowDataContext : ObservableObject
+    {
+        const bool POPULATE_WITH_TEST_DATA = true;
+
+        public MainWindowDataContext()
+        {
+            if(POPULATE_WITH_TEST_DATA)
+            {
+                JobItems.Add(new JobItem { JobName = "Data Backup", StartTime = DateTime.Now.AddMinutes(-30), Status = Status.SUCCESS });
+                JobItems.Add(new JobItem { JobName = "Data Analysis", StartTime = DateTime.Now.AddMinutes(-60), Status = Status.FAILED });
+                JobItems.Add(new JobItem { JobName = "Report Generation", StartTime = DateTime.Now.AddMinutes(-120), Status = Status.SUCCESS });
+            }
+        }
+
+        [ObservableProperty]
+        private bool _isAdvancedMode;
+        public ObservableCollection<JobItem> JobItems { get; } = new ObservableCollection<JobItem>();
+    }
+    partial class MenuDataContext : ObservableObject
+    {
+    }
+
+     class MenuItem
+    {
+        public string? Text { get; set; }
+    }
+    enum Status
+    {
+        SUCCESS,
+        FAILED,
+    }
+    class JobItem
+    {
+        [Description("Job Name")]
+        public string? JobName { get; set; }
+
+        [Description("Start Time")]
+        public DateTime StartTime { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
